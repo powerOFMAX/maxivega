1b2f5bdeffac5f1976a290892fa3c5b912b0c82f
powerOFMAX <max_maxi@live.com.ar>
2023-06-17 16:31:20 +0200

    updated desk and ilumination

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,11 +4,12 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./scene.gltf");
+  // const computer = useGLTF("./escenarioPagina1.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={0.6} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight
         position={[-20, 50, 10]}
@@ -20,9 +21,12 @@ const Computers = ({ isMobile }) => {
       />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        // scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 0.6 : 1.3}
+        position={isMobile ? [1.5, -2, 0.5] : [1.5, -2, -1]}
+        // position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        // rotation={[-0.01, -0.2, -0.1]}
+        rotation={[0.05, 0.49, -0.1]}
       />
     </mesh>
   );
@@ -32,7 +36,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    const mediaQuery = window.matchMedia("(max-width: 800px)");
 
     // set the initial value of isMobile
     setIsMobile(mediaQuery.matches);
@@ -61,6 +65,9 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          //to 45 degrees
+          minAzimuthAngle={-Math.PI / 4 + Math.PI / 2.6} // Adjust the minimum allowed angle
+          maxAzimuthAngle={Math.PI / 4 + Math.PI / 3}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
